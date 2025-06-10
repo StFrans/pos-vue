9520b4e930cd49ccfbad3befb4e79e929b90c579
Luis Angel <sosafloresluisangel@gmail.com>
2025-06-09 22:36:40 -0600

    fea(header): optimizando título del header

--- src/components/Logo.vue
+++ src/components/Logo.vue
@@ -1,8 +1,8 @@
 <template>
     <div>
         <h1 class="text-3xl font-black text-white">
-            POS
-            <span class="text-green-400">Vue.js</span>
+            Moda 
+            <span class="text-green-400">a la medida</span>
         </h1>
     </div>
 </template>
